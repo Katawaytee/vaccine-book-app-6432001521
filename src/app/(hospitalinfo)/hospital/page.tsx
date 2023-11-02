3ced43df5dc1631e762efe01ee1444ae2e90a8ae
Katawaytee <ktriampo@gmail.com>
2023-11-02 21:58:18 +0700

    Assignment 12

--- src/app/(hospitalinfo)/hospital/page.tsx
+++ src/app/(hospitalinfo)/hospital/page.tsx
@@ -2,15 +2,39 @@ import getHospitals from '@/libs/getHospitals';
 import HospitalCatalog from '@/components/HospitalCatalog';
 import { Suspense } from 'react';
 import { LinearProgress } from '@mui/material';
+import AddHospitalForm from '@/components/AddHospitalForm';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from "next-auth/next";
+import getUserProfile from "@/libs/getUserProfile";
 
-export default function Hospital() {
+export default async function Hospital() {
+  
   const hospitals = getHospitals();
-  return(
-    <main className="text-center p-5">
-      <h1 className="text-xl font-medium">Select Hospital</h1>
-      <Suspense fallback={<p>Loading...<LinearProgress/></p>}>
-        <HospitalCatalog hospitalJson={hospitals}/>
-      </Suspense>
-    </main>
-  );
+
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.token) {
+    return(
+      <main className="text-center p-5">
+        <h1 className="text-xl font-medium">Select Hospital</h1>
+        <Suspense fallback={<p>Loading...<LinearProgress/></p>}>
+          <HospitalCatalog hospitalJson={hospitals}/>
+        </Suspense>
+      </main>
+    );
+  } else {
+    const profile = await getUserProfile(session.user.token);
+    return(
+      <main className="text-center p-5">
+        <h1 className="text-xl font-medium">Select Hospital</h1>
+        <Suspense fallback={<p>Loading...<LinearProgress/></p>}>
+          <HospitalCatalog hospitalJson={hospitals}/>
+          {
+            (profile.data.role == "admin")?
+            <AddHospitalForm/>
+            :null
+          }
+        </Suspense>
+      </main>
+    );
+  }
 }
